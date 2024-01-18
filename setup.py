88316a6a1a0a3660ff0be308ca46d643e5e8da9f
Jonathan Taylor <jonathan.taylor@stanford.edu>
2024-01-18 10:46:02 -0800

    editing setup files, cleanup

--- setup.py
+++ setup.py
@@ -55,14 +55,6 @@ info = read_vars_from(pjoin('ISLP', 'info.py'))
 # is missing.  Otherwise the monkeypatched Extension will change .pyx
 # filenames to .c filenames, and we probably don't have the .c files.
 sys.path.insert(0, pjoin(dirname(__file__), 'fake_pyrex'))
-# Set setuptools extra arguments
-extra_setuptools_args = dict(
-    tests_require=['nose'],
-    test_suite='nose.collector',
-    zip_safe=False,
-    extras_require = dict(
-        doc=['Sphinx>=1.0'],
-        test=['nose>=0.10.1']))
 
 # Define extensions
 EXTS = []
@@ -74,20 +66,20 @@ cmdclass=versioneer.get_cmdclass()
 long_description = open('README.md', 'rt', encoding='utf-8').read()
 
 def main(**extra_args):
-    setup(name=info.NAME,
-          maintainer=info.MAINTAINER,
-          maintainer_email=info.MAINTAINER_EMAIL,
-          description=info.DESCRIPTION,
-          url=info.URL,
-          download_url=info.DOWNLOAD_URL,
-          license=info.LICENSE,
-          classifiers=info.CLASSIFIERS,
-          author=info.AUTHOR,
-          author_email=info.AUTHOR_EMAIL,
-          platforms=info.PLATFORMS,
+    setup(#name=info.NAME,
+          #maintainer=info.MAINTAINER,
+          #maintainer_email=info.MAINTAINER_EMAIL,
+          #description=info.DESCRIPTION,
+          #url=info.URL,
+          #download_url=info.DOWNLOAD_URL,
+          #license=info.LICENSE,
+          #classifiers=info.CLASSIFIERS,
+          #author=info.AUTHOR,
+          #author_email=info.AUTHOR_EMAIL,
+          #platforms=info.PLATFORMS,
           version=versioneer.get_version(),
-          requires=info.REQUIRES,
-          provides=info.PROVIDES,
+          #requires=info.REQUIRES,
+          #provides=info.PROVIDES,
           packages     = ['ISLP',
                           'ISLP.models',
                           'ISLP.models',
@@ -108,4 +100,4 @@ def main(**extra_args):
 #simple way to test what setup will do
 #python setup.py install --prefix=/tmp
 if __name__ == "__main__":
-    main(**extra_setuptools_args)
+    main()
